82650cb9ae998354955e479a6b8bed58f533f763
akshra-s <akshraa2007@gmail.com>
2026-07-20 17:31:56 +0530

    Handled Errors

--- StaySync/app.js
+++ StaySync/app.js
@@ -5,6 +5,8 @@ const listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride = require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utility/wrapAsync.js");
+const ExError=require("./utility/ExError.js");
 
 //connection..
 main()
@@ -38,42 +40,58 @@ app.get("/listing/new",(req,res)=>{
 });
 
 //show route..
-app.get("/listing",async(req,res)=>{
+app.get("/listing",wrapAsync(async(req,res)=>{
     const allList=await listing.find({});
     res.render("listing/index.ejs",{allList});
-});
-app.get("/listing/:id",async(req,res)=>{
+}));
+app.get("/listing/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const list=await listing.findById(id);
     res.render("listing/show.ejs",{list});
-});
+}));
 
 //create route..(CREATE the NEW listing II)..
-app.post("/listing",async(req,res)=>{
+app.post("/listing",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw ExError(400,"Send Valid Data !");
+    };
     const newlist=new listing(req.body.listing); 
     await newlist.save();
     res.redirect("/listing");
-});
+}));
 
 //edit route.. (renders a form I)..
-app.get("/listing/:id/edit",async(req,res)=>{
+app.get("/listing/:id/edit",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const list=await listing.findById(id);
     res.render("listing/edit.ejs",{list});
-});
+}));
+
 //update route.. (UPDATE the EDITED listing II)
-app.put("/listing/:id",async(req,res)=>{
+app.put("/listing/:id",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw ExError(400,"Send Valid Data !");
+    };
     let {id} = req.params;
     await listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listing/${id}`);
-});
+}));
 
 //DELETE ROUTE
-app.delete("/listing/:id",async(req,res)=>{
+app.delete("/listing/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let delList=await listing.findByIdAndDelete(id);
     console.log(delList);
     res.redirect("/listing");
+}));
+
+//For All invalid routes..
+app.use((req,res,next)=>{
+    next(new ExError(404,"Page Not Found !"));
+});
+app.use((err,req,res,next)=>{
+    let{status=500,message="Something Went Wrong !"}=err;
+    res.status(status).render("error.ejs",{message});
 });
 
 app.listen(8080,()=>{
