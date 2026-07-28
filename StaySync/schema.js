67d59d2fe1ed54af4e7eb03b5067d5fcb8db959d
akshra-s <akshraa2007@gmail.com>
2026-07-28 23:32:27 +0530

    improve code quality

--- StaySync/schema.js
+++ StaySync/schema.js
@@ -3,7 +3,7 @@ const listingSchema = Joi.object({
     listing:Joi.object({
         title:Joi.string().required().max(100),
         description:Joi.string().required(),
-        image:Joi.string().allow("",null),
+        image:Joi.string().allow("",null).uri(),
         price:Joi.number().required().min(0),
         location:Joi.string().required(),
         country:Joi.string().required(),
