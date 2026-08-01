9641f32c9d916907bf0ffcce06b8d73ceb2951ee
akshra-s <akshraa2007@gmail.com>
2026-08-01 23:44:28 +0530

    improved errors.ejs

--- StaySync/routes/review.js
+++ StaySync/routes/review.js
@@ -2,7 +2,6 @@ const express=require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync=require("../utility/wrapAsync.js");
 const {reviewSchema}=require("../schema.js");
-const {listingSchema}=require("../schema.js");
 const ExError=require("../utility/ExError.js");
 const listing=require("../models/listing.js");
 const review=require("../models/review.js");
@@ -26,6 +25,7 @@ router.post("/",validateReview,wrapAsync(async (req,res)=>{
     list.reviews.push(newReview);
     await newReview.save();
     await list.save();
+    req.flash("success", "Review added successfully!");
     res.redirect(`/listing/${list._id}`);
 }));
 //delete review..
@@ -33,6 +33,7 @@ router.delete("/:reviewid",wrapAsync(async(req,res)=>{
     let {id,reviewid} = req.params;
     await listing.findByIdAndUpdate(id,{$pull:{reviews:reviewid}});
     await review.findByIdAndDelete(reviewid);
+    req.flash("success", "Review deleted successfully!");
     res.redirect(`/listing/${id}`);
 }));
 
