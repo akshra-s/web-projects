2f4d10f807c7cc4116284bb2c811dcf6fde28167
akshra-s <akshraa2007@gmail.com>
2026-08-10 20:04:40 +0530

    Improved Redirection

--- StaySync/middleware.js
+++ StaySync/middleware.js
@@ -1,7 +1,15 @@
 module.exports.isloggedin=(req,res,next)=>{
     if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl;
         req.flash("error","Kindly Login to procceed further !");
         return res.redirect("/login");
+    }
+    next();
+};
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
+        delete req.session.redirectUrl;
     }
     next();
 };
--- StaySync/routes/user.js
+++ StaySync/routes/user.js
@@ -3,34 +3,42 @@ const router = express.Router();
 const user=require("../models/user.js");
 const wrapAsync = require("../utility/wrapAsync.js");
 const passport =require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
-router.post("/signup",wrapAsync(async(req,res)=>{
+router.post("/signup",async(req,res,next)=>{
     try{
         let{username,email,password}=req.body;
         const newUser=new user({email,username});
         const registeredUser =await user.register(newUser,password);
         console.log(registeredUser);
-        req.flash("success","Welcome to StaySync!");
-        res.redirect("/listing");
+        req.login(registeredUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","Welcome to StaySync!");
+            res.redirect("/listing");
+        });
     }
     catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
     }
-}));
+});
 
 router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 });
 router.post("/login",
+    saveRedirectUrl,
     passport.authenticate("local",
         {failureRedirect:"/login",failureFlash:true,}),
         (req,res)=>{
             req.flash("success","Welcome Back To StaySync !");
-            res.redirect("/listing");
+            let redirectUrl=res.locals.redirectUrl || "/listing";
+            res.redirect(redirectUrl);
 });
 router.get("/logout",(req,res,next)=>{
     req.logout((err)=>{
